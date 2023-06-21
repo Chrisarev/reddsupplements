9346b55fef034f1ed24e2bd5b3758f9916a552fe
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-06-21 13:26:46 -0700

    ,.

--- client/viteReact/src/ProductShow.jsx
+++ client/viteReact/src/ProductShow.jsx
@@ -18,33 +18,35 @@ const ProductShow = () => {
             return response.json()
         }).then((data) => {
             console.log(data);
-            setProduct(prev => data);
+            setProduct(data);
         })
     }, [])
 
 
     return (
         <>
-        <Navbar />
-        <div className={styles.panel}>
-            <div className={styles.productInfo}>
-                <div className={styles.productTitle}>
-                    {product.productTitle}
-                </div>
-                <div className={productImage}>
-                    <img src={product.image} alt="" />
-                </div>
-                <div className={styles.productPrice}>
-                    {product.productPrevPrice &&
-                        <span>{product.productPrevPrice}</span>
-                    }
-                    {product.productPrice}
-                </div>
-                <div className={styles.productDesc}>
-                    {product.productDesc}
-                </div>
+            <Navbar />
+            <div className={styles.panel}>
+                {product &&
+                    <div className={styles.productInfo}>
+                        <div className={styles.productTitle}>
+                            {product.productTitle}
+                        </div>
+                        <div className={productImage}>
+                            <img src={product.image} alt="" />
+                        </div>
+                        <div className={styles.productPrice}>
+                            {product.productPrevPrice &&
+                                <span>{product.productPrevPrice}</span>
+                            }
+                            {product.productPrice}
+                        </div>
+                        <div className={styles.productDesc}>
+                            {product.productDesc}
+                        </div>
+                    </div>
+                }
             </div>
-        </div>
         </>
     )
 }

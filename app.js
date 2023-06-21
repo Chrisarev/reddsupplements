99ca38fe0f9761a1f20259dd55b6d02eb07bccf7
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-06-21 15:42:38 -0700

    .

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const path = require('path')
 const mongoose = require('mongoose') ///for connecting to mongoDB
 const methodOverride = require('method-override') ///allows http verbs other than POST/GET in forms
 const mongoSanitize = require('express-mongo-sanitize') ///prevents mongo injection
-const Product = require('./models/product') 
+const Product = require('./models/product')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const multer = require('multer'); ///require multer to parse images sent in forms 
-const {storage} = require('./cloudinary')
-const upload = multer({storage}) ///store uploaded images to cloudinary path in storage config 
+const { storage } = require('./cloudinary')
+const upload = multer({ storage }) ///store uploaded images to cloudinary path in storage config 
 const cors = require('cors')
 const session = require('express-session') ///allows us to make HTTP stateful with individual sessions and cookies
 const passport = require('passport') ///auth middleware
 const LocalStrategy = require('passport-local') ///LOCAL user/pass auth strategy for passport
-const User = require('./models/user') 
+const User = require('./models/user')
 const MongoDBStore = require('connect-mongo') ///allows us to store session in mongo
 const { isLoggedIn } = require('./middleware')
-const {validateUserInfo} = require('./middleware')
+const { validateUserInfo } = require('./middleware')
 
 /***************** DATABASE CONFIG *****************/
 const dbUrl = process.env.DB_URL;
@@ -39,141 +39,142 @@ db.once("open", () => {
 /***************** MIDDLEWARE CONFIG *****************/
 const app = express()
 app.use(cors())
-app.use(express.json({limit:"50mb"}))
+app.use(express.json({ limit: "50mb" }))
 app.use(express.static(path.join(__dirname + '/public')))
 app.use(express.urlencoded({ extended: true })) ///allows us to get req.params 
 app.use(methodOverride('_method')) ///allows requests other than get/post thru forms 
 app.use(mongoSanitize()) ///prevents users from inputting characters that could result in mongo injection
 
-/***************** SESSION CONFIGURATION *****************/ 
+/***************** SESSION CONFIGURATION *****************/
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = MongoDBStore.create({
-    mongoUrl:dbUrl,
+    mongoUrl: dbUrl,
     secret,
     touchAfter: 24 * 60 * 60 ///lazy update session unless 1 day has passed
 });
 
-store.on('error', function(e){
+store.on('error', function (e) {
     console.log('SESSION STORE ERROR:', e)
 })
 
-const sessionConfig = { 
-    store, 
-    name:'__umli', 
+const sessionConfig = {
+    store,
+    name: '__umli',
     secret, ///used to sign cookies
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        httpOnly:true, ///cross-site scripting protection
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true, ///cross-site scripting protection
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,  ///expires a week from time created 
         maxAge: 1000 * 60 * 60 * 24 * 7 ///maxAge of cookie is a week
     }
 }
 app.use(session(sessionConfig))
 
-/***************** PASSPORT CONFIGURATION *****************/ 
+/***************** PASSPORT CONFIGURATION *****************/
 app.use(passport.initialize())
 app.use(passport.session()) ///for persistent login sessions
 passport.use(new LocalStrategy(User.authenticate())) ///use UserSchema authentication that was plugged into User Schema with passport-local-mongoose
-passport.deserializeUser(User.deserializeUser()) 
+passport.deserializeUser(User.deserializeUser())
 
-/***************** ROUTES *****************/ 
-app.get('/api/category/:category', async (req,res) =>{
+/***************** ROUTES *****************/
+app.get('/api/category/:category', async (req, res) => {
     console.log('Retrieving all' + req.params.category + ' products.')
-    const products = await Product.find({'productCategory' : req.params.category}).populate();
+    const products = await Product.find({ 'productCategory': req.params.category }).populate();
     res.json(products)
 })
 
 
-app.get('/api/products', async (req,res) =>{
+app.get('/api/products', async (req, res) => {
     console.log('Retrieving all products.')
-    const products = await Product.find({}).populate(); 
-    res.json(products); 
+    const products = await Product.find({}).populate();
+    res.json(products);
 })
 
-app.get('/api/product/:prodID', async (req,res) =>{
+app.get('/api/product/:prodID', async (req, res) => {
     const productID = req.params.prodID;
-    const product = new Product(); 
-    try{
-     product = await Product.findById({_id: productID}); 
-     res.json(product);
+    console.log('productID: ' + productID)
+    try {
+        const product = await Product.findById({ _id: productID });
+        res.json(product);
     } catch (e) {
-        console.log('failed') 
+        console.log('failed')
         res.sendStatus(301)
-    }})
+    }
+})
 
-app.get('/api/getCart', async (req,res) =>{
+app.get('/api/getCart', async (req, res) => {
     res.sendStatus(203);
 })
 
 /* adding a new Product */
-app.post('/api/addProduct', upload.any(), async (req,res) =>{
-    const formData = req.body; 
+app.post('/api/addProduct', upload.any(), async (req, res) => {
+    const formData = req.body;
     console.log(formData);
-    try{
-        const newProduct = new Product();         
-        if(req.files){
-            newProduct.image = req.files[0].path; 
+    try {
+        const newProduct = new Product();
+        if (req.files) {
+            newProduct.image = req.files[0].path;
         }
         newProduct.productTitle = formData.productTitle;
         newProduct.productDesc = formData.productDesc;
-        newProduct.productCategory = formData.productCategory; 
-        newProduct.productPrice = formData.productPrice; 
+        newProduct.productCategory = formData.productCategory;
+        newProduct.productPrice = formData.productPrice;
         newProduct.productPrevPrice = formData.productPrevPrice;
         console.log('trying 3!')
-        await newProduct.save(); 
+        await newProduct.save();
         console.log('POSTED!')
-        res.sendStatus(204); 
-    }catch (e) {
+        res.sendStatus(204);
+    } catch (e) {
         console.log('FAILED!')
         res.sendStatus(301);
     }
 })
 
 /* Checking to see if user is logged in */
-app.get('/api/sAuth', isLoggedIn, (req,res) =>{
+app.get('/api/sAuth', isLoggedIn, (req, res) => {
     res.sendStatus(204);
 })
 
 /* Registering a new User*/
-app.post('/api/postUser', catchAsync(async (req,res) =>{
-    try{
-        const {email, username, password} = req.body; 
-        const user = new User({email,username})
-        const registeredUser = await User.register(user,password);
-        req.login(registeredUser , err =>{
-            if(err) return next(err);
-            res.json({redirectURL:'/', user: username})
+app.post('/api/postUser', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username })
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            res.json({ redirectURL: '/', user: username })
         })
     } catch (e) {
-        res.json({redirectURL: 'signup'})
+        res.json({ redirectURL: 'signup' })
     }
 }))
 
 /* attempting to login for existing user */
-app.post('/api/login', passport.authenticate('local',{ keepSessionInfo:true}), (req,res) =>{
+app.post('/api/login', passport.authenticate('local', { keepSessionInfo: true }), (req, res) => {
     if (!req.user) {
         res.sendStatus(401)
-    }else {
-        res.json({user:req.user})
+    } else {
+        res.json({ user: req.user })
     }
 })
 
 /* attempting to logOut for existing user*/
-app.post('/api/logout', (req,res) =>{
+app.post('/api/logout', (req, res) => {
     req.logout(function (err) {
-        if (err) {res.json({attempt:"Failed"})}
-        res.json({attempt:'Success'})
+        if (err) { res.json({ attempt: "Failed" }) }
+        res.json({ attempt: 'Success' })
     })
 })
 
-app.get('/api/cart/:userID', isLoggedIn, (req,res) =>{
+app.get('/api/cart/:userID', isLoggedIn, (req, res) => {
     res.sendStatus(204);
 })
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+ '/public/index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 const PORT = process.env.PORT || 3000;

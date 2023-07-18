e70aff5a1f7b30091f2fd02c7a8fe8cc5a836258
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-07-18 14:44:20 -0700

    Made productShow component responsive, made home page slider slideCount responsive.

--- client/viteReact/src/Home.jsx
+++ client/viteReact/src/Home.jsx
@@ -7,11 +7,42 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import goldProtein from './stylesheets/images/goldProtein.jpg'
+import dynaProtein from './stylesheets/images/dymatizeProtein.jpg'
 import Footer from './Footer'
 import cBumBanner from './stylesheets/images/cbumBanner.jpg'
 import { Link } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { useState } from 'react'
 const Home = () => {
+    const [slideCount, setSlideCount] = useState(4);
+
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+            width,
+            height
+        };
+    }
+
+    /* windowDimensions state that holds window width and height*/
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    /* This useEffect activates whenever window is resized */
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    function getSlideValue() {
+        if (windowDimensions.width < 1200) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
 
     return (
         <div className={styles.panel}>
@@ -49,7 +80,7 @@ const Home = () => {
                 <Swiper
                     modules={[Navigation, Pagination, Scrollbar, A11y]}
                     spaceBetween={30}
-                    slidesPerView={4}
+                    slidesPerView={getSlideValue()}
                     navigation
                     pagination={{ clickable: true }}
                     scrollbar={{ draggable: true }}
@@ -57,48 +88,59 @@ const Home = () => {
                     onSlideChange={() => console.log('Slide change')}
                 >
                     <SwiperSlide>
-                        <div className={styles.productHolder}>
-                            <img src={goldProtein} alt="" />
+                        <Link to='/product/dsadsadsas' className={styles.productHolder}>
+                            <div className={styles.imgHolder}>
+                                <img src={goldProtein} alt="" />
+                            </div>
                             <p>Gold's Protein</p>
                             <p className={styles.productPrice}>$30.00</p>
-                        </div>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className={styles.productHolder}>
-                            <img src={goldProtein} alt="" />
-                            <p>Gold's Protein</p>
-                            <p className={styles.productPrice}>$30.00</p>
-                        </div>
+                        <Link to='/product/64b48061e6de85ae357e1884' className={styles.productHolder}>
+                            <div className={styles.imgHolder}>
+                                <img src={dynaProtein} alt="" />
+                            </div>
+                            <p>Dymatize Protein Powder - Rich Chocolate Flavor</p>
+                            <p className={styles.productPrice}>
+                                <span className={styles.preDiscountPrice}>$67.00</span>
+                                $62.00
+                            </p>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className={styles.productHolder}>
-                            <img src={goldProtein} alt="" />
+                        <Link to='/product/dsadsadsas' className={styles.productHolder}>
+                            <div className={styles.imgHolder}>
+                                <img src={goldProtein} alt="" />
+                            </div>
                             <p>Gold's Protein</p>
                             <p className={styles.productPrice}>
                                 <span className={styles.preDiscountPrice}>$35.00</span>
                                 $30.00</p>
-                        </div>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className={styles.productHolder}>
+                        <Link to='/product/dsadsadsas' className={styles.productHolder}>
+                            <div className={styles.imgHolder}>
+                                <img src={goldProtein} alt="" />
+                            </div>
+                            <p>Gold's Protein</p>
+                            <p className={styles.productPrice}>$30.00</p>
+                        </Link>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Link to='/product/dsadsadsas' className={styles.productHolder}>
                             <img src={goldProtein} alt="" />
                             <p>Gold's Protein</p>
                             <p className={styles.productPrice}>$30.00</p>
-                        </div>
+                        </Link>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <div className={styles.productHolder}>
+                        <Link to='/product/dsadsadsas' className={styles.productHolder}>
                             <img src={goldProtein} alt="" />
                             <p>Gold's Protein</p>
                             <p className={styles.productPrice}>$30.00</p>
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <div className={styles.productHolder}>
-                            <img src={goldProtein} alt="" />
-                            <p>Gold's Protein</p>
-                            <p className={styles.productPrice}>$30.00</p>
-                        </div>
+                        </Link>
                     </SwiperSlide>
                 </Swiper>
             </div>

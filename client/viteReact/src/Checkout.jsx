a1699b5e7986e0f29394d43f8dded5801964a41d
Chrisarev <CHRIS.AREV@GMAIL.COM>
2023-08-03 17:09:00 -0700

    .

--- client/viteReact/src/Checkout.jsx
+++ client/viteReact/src/Checkout.jsx
@@ -27,7 +27,7 @@ const Checkout = () => {
             console.log(data[0].products)
             setCart(prev => arr)
             for(let i=0; i < cart.length; i++ ){
-                setSubtotal(prev => prev + cart[i].productPrice.$numberDecimal)
+                setSubtotal(prev => prev + cart[i].productPrice)
             }
         })
     }, [])
